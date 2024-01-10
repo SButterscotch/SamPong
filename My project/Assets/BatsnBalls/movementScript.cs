6fe373e663d3c4b6c5eafdd598d3bd69b627a6b8
Sambhav <ssambhavv@gmail.com>
2024-01-09 20:47:40 -0800

    glow
    glow with player movement

--- My project/Assets/BatsnBalls/movementScript.cs
+++ My project/Assets/BatsnBalls/movementScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
+    static public bool moving;
     public Rigidbody2D rb;
     public float moveSpeed = 1000f;
     public int targetFPS = 360;
@@ -21,7 +22,14 @@ public class MovementScript : MonoBehaviour
 
     void HandleMovementInput()
     {
-        float verticalInput = Input.GetKey(KeyCode.W) ? 50f : (Input.GetKey(KeyCode.S) ? -50f : 0f);
+        float verticalInput = Input.GetKey(KeyCode.W) ? 50f : Input.GetKey(KeyCode.S) ? -50f : 0f;
+        if (Mathf.Abs(verticalInput) > 0) 
+        {
+            moving = true;
+        }else
+        {
+            moving = false;
+        }
         rb.velocity = new Vector2(rb.velocity.x, verticalInput * moveSpeed * Time.deltaTime);
     }
 }
---
+++ My project/Assets/glow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class glow : MonoBehaviour
+{
+    public PostProcessVolume gloww;
+    private Bloom lighting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gloww = GetComponent<PostProcessVolume>();
+        gloww.profile.TryGetSettings(out lighting);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool move = MovementScript.moving;
+        if (move)
+        {
+            lighting.intensity.value = 20;
+        }else{
+            lighting.intensity.value = 0;
+        }
+    }
+}
